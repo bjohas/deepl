b3b4e657af8c56ff13e2291ed0d5cdbd121bac0e
Björn Haßler <bjohas+git@gmail.com>
2020-09-13 15:40:52 +0100

    tweaks

--- deepl.js
+++ deepl.js
@@ -21,33 +21,55 @@ parser.add_argument("text", { help: "Text to be translated. (Required. Or use -u
 parser.add_argument('-s','--source', { help: "Source language. One of: DE, EN-GB, EN-US, FR, IT, JA, ES, NL, PL, PT-PT, PT-BR, RU, ZH. Default: en. You can use lower case.", default: "" });
 parser.add_argument('-t','--target', { help: "Target language. Same options as for source language. (Required.)" });
 parser.add_argument('-f','--formality', { help: "Formality: default, more, less. For all languages except EN-GB, EN-US, ES, JA, ZH.", default: "default" });
-parser.add_argument('--de', { help: "Translate to informal German.", action: 'store_true' });
-parser.add_argument('--fr', { help: "Translate to informal French.", action: 'store_true' });
-parser.add_argument('--pt', { help: "Translate to informal Portuguese.", action: 'store_true' });
-parser.add_argument('--br', { help: "Translate to informal Brazilian.", action: 'store_true' });
+parser.add_argument('--de', { help: "Translate to German (-t de).", action: 'store_true' });
+parser.add_argument('--fr', { help: "Translate to French (-t fr).", action: 'store_true' });
+parser.add_argument('--pt', { help: "Translate to Portuguese (-t pt-pt).", action: 'store_true' });
+parser.add_argument('--br', { help: "Translate to Brazilian (-t pt-br).", action: 'store_true' });
+parser.add_argument('--dei', { help: "Translate to informal German (-t de -f less).", action: 'store_true' });
+parser.add_argument('--fri', { help: "Translate to informal French (-t fr -f less).", action: 'store_true' });
+parser.add_argument('--pti', { help: "Translate to informal Portuguese (-t pt-pt -f less).", action: 'store_true' });
+parser.add_argument('--bri', { help: "Translate to informal Brazilian (-t pt-br -f less).", action: 'store_true' });
 parser.add_argument('-u','--usage', { help: "Get usage.", action: 'store_true' });
 parser.add_argument('-n','--nocopytoclipboard', { help: "Do not copy result to clipboard.", action: 'store_true' });
+parser.add_argument('-k','--keeporiginal', { help: "Print out the original text / copy original text to clipboard.", action: 'store_true' });
 var arg = parser.parse_args();
 
-if (arg["de"]) {
+if (arg["dei"]) {
   arg["target"] = "DE";
   arg["formality"] = "less";
 };
 
-if (arg["fr"]) {
+if (arg["fri"]) {
   arg["target"] = "FR";
   arg["formality"] = "less";
 };
 
-if (arg["pt"]) {
+if (arg["pti"]) {
   arg["target"] = "PT-pt";
   arg["formality"] = "less";
 };
 
-if (arg["br"]) {
+if (arg["bri"]) {
   arg["target"] = "PT-BR";
   arg["formality"] = "less";
 };
+
+if (arg["de"]) {
+  arg["target"] = "DE";
+};
+
+if (arg["fr"]) {
+  arg["target"] = "FR";
+};
+
+if (arg["pt"]) {
+  arg["target"] = "PT-pt";
+};
+
+if (arg["br"]) {
+  arg["target"] = "PT-BR";
+};
+
 
 if (!arg["text"] && !arg["usage"]) {
   console.log("TEXT is required");
@@ -104,11 +126,20 @@ async function translateTextDeepL(txt,from,to,formality) {
 
 (async () => {
   if (!arg["usage"]) {
-    const text = await translateTextDeepL(arg["text"],arg["source"],arg["target"],arg["formality"]);
+    var text = await translateTextDeepL(arg["text"],arg["source"],arg["target"],arg["formality"]);
+    var textcb = text;
+    if (arg["keeporiginal"]) {
+      if (!arg["source"]) {
+	arg["source"] = "?";
+      };
+      text = arg["source"]+": "+arg["text"]+"\n"+arg["target"]+": "+text;
+      // arg["text"] = arg["text"].slice(0, -1);
+      textcb = textcb+" [\""+arg["text"]+"\"]";
+    };
     console.log(text);
     if (arg["nocopytoclipboard"]) {
     } else {
-      clipboardy.writeSync(text);
+      clipboardy.writeSync(textcb);
     };
   } else {
     const text = await getDeepLUsage();
